0d75f2716654211017665bb55ef1a0965ec0ee72
Charlie Stanard <charlie@bokkagroup.com>
2017-02-13 13:26:44 -0700

    Adding an alerts utility

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bokka WordPress Utilities
 
-This plugin should be run from the `mu-plugins` folder. For more information on must use plugins see the [WordPress Codex](https://codex.wordpress.org/Must_Use_Plugins).
+Intended to provide global utilities for themes and plugins. Intended to be used with the Bokka WP Theme and Bokka MVC plugin. This plugin should be run from the `mu-plugins` folder. For more information on must use plugins see the [WordPress Codex](https://codex.wordpress.org/Must_Use_Plugins).
 
 ## Usage
 
---
+++ utilities/Alerts.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BokkaWP\Utilities;
+
+class Alerts {
+    public function __construct()
+    {
+
+    }
+
+    public function send($errors = array(), $severity = "warning", $app = false){
+
+            if(count($errors) < 1){
+                error_log('Bokka Utilities: Alerts Empty array of errors');
+                return;
+            }
+
+            $app = $app ? $app : $_SERVER['HTTP_HOST'];
+             // create curl resource
+            $url = "http://138.68.19.98:8080/incoming-hooks/errors";
+            $data = array(
+                'app'   => $app,
+                'level' => $severity,
+                'url'   =>  $_SERVER['HTTP_HOST'],
+                'errors' =>  $errors,
+            );
+            $post = json_encode($data);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($post))
+            );
+
+            $result = curl_exec($ch);
+            return $result;
+    }
+
+    public static function get_instance()
+    {
+        if (null === static::$instance) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+}
+
+$Alerts = Alerts::get_instance();

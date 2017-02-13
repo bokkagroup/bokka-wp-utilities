e59c0dfd0029ec98d98b84abaca1bf8ef962cf10
Charlie Stanard <charlie@bokkagroup.com>
2017-02-13 12:56:15 -0700

    Removing empty files

--- utilities/errors.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/**
- * Thinking we could have an error handler
- * $error = new BokkaWP\Utilities\Error(msg, type);
- * Where msg is the specific issue, and type can be a severity
- * We can prepend the namespace/class easily
- */
